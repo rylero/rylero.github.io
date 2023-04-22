5ec67282568aca988a643fae789b424720d9f433
Ryan LeRoy <leroy.ryan09@gmail.com>
2023-04-22 16:34:50 -0700

    fix spelling

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   manifest: true,
-  base: './rylero.github.io/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
